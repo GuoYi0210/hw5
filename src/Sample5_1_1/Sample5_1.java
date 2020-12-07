e78d035bfa2b562c6c77ecfa4bb190eee50ecd2f
GuoYi0210 <a0930270656@gmail.com>
2020-12-07 23:35:00 +0800

    first

---
+++ src/Sample5_1_1/Sample5_1.java
@@ -0,0 +1,11 @@
+package Sample5_1_1;
+
+class Sample5_1 
+{
+	public static void main(String[] args)
+	 {
+	  Car car1;
+	  car1 = new Car();
+	  car1.show();
+	 }
+}
---
+++ src/Sample5_5/Sample5_5_2.java
@@ -0,0 +1,13 @@
+package Sample5_5;
+
+class Sample5_5_2 
+{
+	public static void main(String[] args)throws CarException
+	 {
+	  Car car1;
+	  car1 = new Car();
+	  car1.setCar(1234, -10.0);
+	  
+	  car1.show();
+	 }
+}
